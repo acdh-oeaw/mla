81304925d6337b286cf5a7b9e0f3d06ed8698ab0
Max Theisen <theisenmax@gmail.com>
2020-02-13 12:55:18 +0100

    Added manuscripta request test.

--- tests/Feature/ImageTest.php
+++ tests/Feature/ImageTest.php
@@ -2,12 +2,21 @@
 
 namespace Tests\Feature;
 
+use GuzzleHttp\Client;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ImageTest extends TestCase
 {
+    public function test_manuscripta_request() {
+        $client = new Client([ 'base_uri' => 'http://manuscripta.at/images/AT/1000/AT1000-25/1/' ]);
+        $response = $client->request('GET', 'AT1000-25_017r.jpg', [ 'sink' => storage_path('app/test') ]);
+        $size = filesize(storage_path('app/test'));
+        unlink(storage_path('app/test'));
+        $this->assertTrue($size == 481276);
+    }
+
     /**
      * Test if codex images are loaded correctly.
      *
@@ -15,7 +24,7 @@ class ImageTest extends TestCase
      */
     public function test_codex_image_reponse() {
         $response = $this->get('/image/codex/8/174r');
-        $response->assertstatus(200);
+        $response->assertStatus(200);
     }
 
     /**
@@ -25,7 +34,7 @@ class ImageTest extends TestCase
      */
     public function test_unavailable_codex_image_reponse() {
         $response = $this->get('/image/codex/10/21r');
-        $response->assertstatus(200);
+        $response->assertStatus(200);
     }
 
     /**
@@ -35,7 +44,7 @@ class ImageTest extends TestCase
      */
     public function test_tilesource_reponse() {
         $response = $this->get('/dzi/1/1');
-        $response->assertstatus(200);
+        $response->assertStatus(200);
     }
 
     /**
@@ -45,6 +54,6 @@ class ImageTest extends TestCase
      */
     public function test_tile_reponse() {
         $response = $this->get('/dzi/2/6_files/11/1_1.jpg');
-        $response->assertstatus(200);
+        $response->assertStatus(200);
     }
 }
